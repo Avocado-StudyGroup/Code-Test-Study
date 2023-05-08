18e76cddca9985fdba499765e89f1d904a161052
hyerani <hran9404@naver.com>
2023-05-08 22:38:14 +0900

    키보드

--- LeeHyeRan/array/index.js
+++ LeeHyeRan/array/index.js
@@ -1,9 +1,15 @@
 // 키보드
+// 첫번째 for문에서 대문자먼저 a배열에 추가, 중복된 문자가 없으면 a배열에 추가
+// 두번째 for문에서 대문자가 하나라도 있으면 shift키를 "1"로 추가
+// 대문자가 여러개여도 shift 키는 한개로 세야 하기 때문에, "1"이 이미 배열에 있으면 추가하지 않음
 function solution1(s, k) {
   let answer = true;
   let a = [];
   for (let i = 0; i < s.length; i++) {
-    if (!a.includes(s[i]) && !a.includes(s[i].toLowerCase())) {
+    if (s[i] === s[i].toUpperCase()) {
+      a.push(s[i]);
+    }
+    if (!a.includes(s[i]) && !a.includes(s[i].toUpperCase())) {
       a.push(s[i]);
     }
   }
@@ -12,7 +18,8 @@ function solution1(s, k) {
       a.push("1");
     }
   }
+  console.log(a);
   return a.length === k ? answer : (answer = false);
 }
 
-console.log(solution1("TeacHer", 7));
+console.log(solution1("aPplEe", 5));
