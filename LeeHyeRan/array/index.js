bd30cfc40b43597638e4e5352d4d2978eab5b8ef
hyerani <hran9404@naver.com>
2023-05-09 00:49:13 +0900

    수열의 경우 수, 최대값 위치

--- LeeHyeRan/array/index.js
+++ LeeHyeRan/array/index.js
@@ -18,8 +18,66 @@ function solution1(s, k) {
       a.push("1");
     }
   }
-  console.log(a);
   return a.length === k ? answer : (answer = false);
 }
 
 console.log(solution1("aPplEe", 5));
+
+// 수열의 경우 수
+// 답지 참고했습니다..!!
+function solution2(nums) {
+  let answer = 0;
+  let peak = [];
+  for (let i = 1; i < nums.length - 1; i++) {
+    if (nums[i] > nums[i - 1] && nums[i] > nums[i + 1]) {
+      peak.push(i);
+    }
+  }
+  for (let p of peak) {
+    let left = p;
+    let right = p;
+    let lcnt = 0;
+    let rcnt = 0;
+    while (left > 0 && nums[left] > nums[left - 1]) {
+      lcnt++;
+      left--;
+    }
+    while (right < nums.length - 1 && nums[right] > nums[right + 1]) {
+      rcnt++;
+      right++;
+    }
+    answer += lcnt * rcnt;
+  }
+  return answer;
+}
+
+console.log(solution2([1, 2, 3, 4, 5, 3, 2, 1]));
+
+// 최댓값 위치
+function solution3(nums) {
+  let answer = [];
+  let max = 0;
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums[i].length; j++) {
+      if (nums[i][j] > max) {
+        max = nums[i][j];
+        answer = [i + 1, j + 1];
+      }
+    }
+  }
+  return answer;
+}
+
+console.log(
+  solution3([
+    [3, 23, 85, 34, 17, 74, 25, 52, 65],
+    [10, 7, 39, 42, 88, 52, 14, 72, 63],
+    [87, 42, 18, 78, 53, 45, 18, 84, 53],
+    [34, 28, 64, 85, 12, 16, 75, 36, 55],
+    [21, 77, 45, 35, 28, 75, 90, 76, 1],
+    [25, 87, 65, 15, 28, 11, 37, 28, 74],
+    [65, 27, 75, 41, 7, 89, 78, 64, 39],
+    [47, 47, 70, 45, 23, 65, 3, 41, 44],
+    [87, 13, 82, 38, 31, 12, 29, 29, 80],
+  ])
+);
