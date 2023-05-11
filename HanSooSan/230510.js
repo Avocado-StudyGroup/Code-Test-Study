cff467e7cad1f686eee06e5a63944e27bd3abd88
0nesan <0nesan.o8o@gmail.com>
2023-05-11 23:50:39 +0900

    병합

--- HanSooSan/230510.js
+++ HanSooSan/230510.js
@@ -1,247 +1,102 @@
-// 격자판 최대합
-const solution1 = (board) => {
-  let answer = 0;
+// 올바른 괄호
+const solution1 = (s) => {
+  let answer = 'YES';
+  let stack = []
 
-  for (let i = 0; i < board.length; i++) {
-    let [x, y, xy, yx] = [0, 0, 0, 0];
-
-    // -> 왼,오,왼오 대각선 합친 값 구하기
-    for (let j = 0; j < board[i].length; j++) {
-      x += board[i][j];
-      y += board[j][i];
-      xy += board[j][j];
-    }
-
-    // -> 오왼 대각선 합친 값 구하기
-    for (let j = board.length - 1; j >= 0; j--) {
-      yx += board[i][j];
-    }
-
-    // answer과 각 값들 비교해 업데이트
-    answer = Math.max(x, y, xy, yx, answer);
+  // "(" 일 때에는 넣고 ")" 일 때는 넣은걸 빼준다. 
+  // 순서대로 해줄 경우 괄호가 정상적으로 닫힐 때 마다 stack.pop()
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === ')') stack.pop()
+    else stack.push(s[i])
   }
 
+  // 만일 정상적이지 않은 괄호라면 stack에 괄호가 남는다. 그럼 NO
+  if (stack.length !== 0) answer = 'NO'
   return answer;
-};
+}
 
-console.log(
-  solution1([
-    [57, 65, 22, 33, 11, 11],
-    [15, 66, 77, 34, 21, 13],
-    [55, 12, 11, 55, 11, 55],
-    [54, 33, 66, 88, 11, 22],
-    [88, 99, 12, 16, 18, 33],
-    [11, 100, 22, 68, 88, 17],
-  ])
-);
+console.log(solution1("(())()"))
 
-// 스카이 라인
-const solution2 = (board) => {
-  let answer = 0;
+// 괄호문자제거
+const solution2 = (s) => {
+  let stack = [];
 
-  // 스카이 라인 x축 y축 구하기
-  let x = [];
-  let y = [];
-
-  for (let i = 0; i < board.length; i++) {
-    let [maxY, maxX] = [0, 0];
-
-    for (let j = 0; j < board.length; j++) {
-      if (maxX < board[i][j]) maxX = board[i][j];
-      if (maxY < board[j][i]) maxY = board[j][i];
-    }
-
-    x.push(maxX);
-    y.push(maxY);
-  }
-
-  // 스카이 라인 x,y축 중 가장 낮은 층 수에서 해당층 수를 빼면서 answer에 추가
-  for (let i = 0; i < board.length; i++) {
-    for (let j = 0; j < board.length; j++) {
-      answer += Math.min(x[i], y[j]) - board[i][j];
-    }
-  }
-
-  return answer;
-};
-
-console.log(
-  solution2([
-    [2, 5, 7, 3, 5],
-    [6, 8, 9, 7, 3],
-    [3, 2, 4, 5, 7],
-    [7, 2, 5, 8, 6],
-    [1, 2, 3, 4, 5],
-  ])
-);
-
-// 봉우리
-const solution3 = (board) => {
-  let answer = 0;
-  let n = board.length;
-
-  // 좌표 초기화
-  let dx = [-1, 0, 1, 0];
-  let dy = [0, 1, 0, -1];
-
-  // board의 갯수 대로 진행 만일 board가 정사각형이 아니라면 j는 board[i].length 까지 돌아야함
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < n; j++) {
-      // 봉우리가 맞을 때를 체크하기 위한 변수
-      let chk = true;
-      for (let k = 0; k < dx.length; k++) {
-        // nx,ny 좌표는 동서남북 좌표를 돌면서 체크하기 위함
-        let nx = i + dx[k];
-        let ny = j + dy[k];
-
-        // nx>=0 && nx<n && ny>=0 && ny<n 이 반대의 경우는 board를 넘어가는 것
-        // board[nx][ny]>=board[i][j] : 동서남북이 다 작아야 하기에 크거나 같아도 안됨
-        if (nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] >= board[i][j]) {
-          chk = false;
-          break;
-        }
+  for (let i = 0; i < s.length; i++) {
+    // 계속 push 하다가 닫는 괄호가 나오면 여는 괄호가 나올 때 까지 전부 pop해준다
+    if (s[i] === ")") {
+      while (stack[stack.length - 1] !== ('(')) {
+        stack.pop()
       }
-      // 봉우리가 맞다면 chk의 변화는 없으므로 answer을 추가한다.
-      if (chk) answer++;
+      // 여는 괄호가 남아있으면 안되기 때문에 pop한 번 더 해준다.
+      stack.pop()
+    } else stack.push(s[i])
+  }
+
+  return stack.join('')
+}
+
+console.log(solution2("(A(BC)D)EF(G(H)(IJ)K)LM(N)"))
+
+// while 문에 메서드를 호출 할 수 있다는 사실을 안 이후에 다시 풀이 (괄호문자제거)
+const solution3 = (s) => {
+  let stack = []
+
+  // 순서가 특별히 필요하지 않은 경우 for of문이 더 간편하다.
+  for (let x of s) {
+    if (x === ')') {
+      // stack.pop을 계속 해주면서 그 대상이 "(" 이 되면 끝난다. 메서드가 먼저 실행되기 때문에 지운 이후에 멈춘다.
+      while (stack.pop() !== '(');
+    } else stack.push(x)
+  }
+
+  return stack.join('');
+}
+
+console.log(solution3("(A(BC)D)EF(G(H)(IJ)K)LM(N)"))
+
+// 연속된 문자 지우기
+const solution4 = (s) => {
+  let stack = [];
+
+  for (let x of s) {
+    // 스택 배열 마지막이 현 값과 같으면 넣지 않고 pop 
+    if (stack[stack.length - 1] === x) stack.pop()
+    else stack.push(x);
+  }
+
+  return stack.join('');
+}
+
+console.log(solution4("bacccaba"))
+
+// 쇠막대기 
+const solution5 = (s) => {
+  let answer = 0
+  let stack = []
+
+  for (let i = 0; i < s.length; i++) {
+    // "(" 는 일단 push
+    if (s[i] === '(') {
+      stack.push(s[i])
+      // 만일 ")"가 나왔는데 그 바로 전이 "(" 였다면 레이저 이므로 
+      // stack의 마지막을 pop해주고 stack의 길이만큼 answer에 추가
+    } else if (s[i - 1] === '(') {
+      console.log(stack)
+      stack.pop()
+      answer += stack.length
+    } else {
+      // ")"가 나왔는데 이전에도 ")" 였다면 레이저가 아니므로 pop이후 answer 하나 추가
+      stack.pop()
+      answer++
     }
   }
 
   return answer;
-};
-
-console.log(
-  solution3([
-    [5, 3, 7, 2, 3],
-    [3, 7, 1, 6, 1],
-    [7, 2, 5, 3, 4],
-    [4, 3, 6, 4, 1],
-    [8, 7, 3, 5, 2],
-  ])
-);
-
-// 로봇이동
-const solution4 = (board, k) => {
-  let answer = [];
-  let n = board.length;
-  // 방향을 결정지을 변수
-  let d = 1;
-  // 카운트 변수 생성
-  let count = 0;
-  // 좌표 초기화
-  let dx = [-1, 0, 1, 0];
-  let dy = [0, 1, 0, -1];
-  // 현 좌표
-  let [x, y] = [0, 0];
-  // 이동 좌표
-  let [nx, ny] = [0, 0];
-
-  while (count < k) {
-    // 카운트 올려주고, 좌표 이동 시키기
-    count++;
-    [nx, ny] = [x + dx[d], y + dy[d]];
-
-    // nx < 0 || nx >= n || ny < 0 || ny >= n 이 경우는 board 바깥으로 나간 케이스
-    // board[nx][ny] == 1 장애물에 걸린 케이스
-    if (nx < 0 || nx >= n || ny < 0 || ny >= n || board[nx][ny] == 1) {
-      // 좌표를 바꿔주기위해 d를 ++ 5이상 올라가면 안되기에 %4
-      d = (d + 1) % 4;
-      // 위 두 케이스에 걸릴 경우 좌표 방향만 돌려주고 넘어간다 (방향 돌리는 것도 count ++)
-      continue;
-    }
-    // 좌표를 이동시켰는데도 위 케이스에 걸리지 않는다면 그 위치로 이동 할 수 있다는 얘기니 현 좌표를 변경해준다.
-    [x, y] = [nx, ny];
-  }
-  // 마지막으로 나온 현 좌표를 answer에 넣어주면 끝
-  answer.push(x);
-  answer.push(y);
-
-  return answer;
-};
-
-console.log(
-  solution4(
-    [
-      [0, 0, 0, 0, 0],
-      [0, 1, 1, 0, 0],
-      [0, 0, 0, 0, 0],
-      [1, 0, 1, 0, 1],
-      [0, 0, 0, 0, 0],
-    ],
-    10
-  )
-);
-
-// 프로그래머스 공원 산책
-const solution10 = (park, routes) => {
-  let answer = [];
-
-  let dx = [-1, 0, 1, 0];
-  let dy = [0, 1, 0, -1];
-
-  let nx, ny;
-  let x = 0,
-    y = 0;
-  let d;
-
-  // 스타트 지점 찾기
-  for (let i = 0; i < park.length; i++) {
-    for (let j = 0; j < park[0].length; j++) {
-      if (park[i][j] === "S") {
-        [x, y] = [i, j];
-      }
-    }
-  }
-
-  // 이동 좌표 체크
-  let cntX = x,
-    cntY = y;
-
-  // 명령 횟수만큼 for문 돌리기
-  for (let i = 0; i < routes.length; i++) {
-    // 카운트 초기화
-    let count = 0;
-
-    // 방향 초기화
-    switch (routes[i][0]) {
-      case "N":
-        d = 0;
-        break;
-      case "S":
-        d = 2;
-        break;
-      case "W":
-        d = 3;
-        break;
-      case "E":
-        d = 1;
-        break;
-      default:
-        return;
-    }
-
-    // 카운트동안 돌면서 좌표 체크 (카운트는 명령 배열 인덱스2의 숫자)
-    while (count < routes[i][2]) {
-      count++;
-
-      // 이동 좌표 업데이트
-      [nx, ny] = [x + dx[d], y + dy[d]];
-
-      // 막히는 구간이 있으면 멈추고 좌표를 while문 전으로 돌린다.
-      if (nx < 0 || ny < 0 || nx >= park[0].length || ny >= park[0].length || park[nx][ny] === "X") {
-        [x, y] = [cntX, cntY];
-        break;
-      }
-
-      [x, y] = [nx, ny];
-    }
-    [cntX, cntY] = [x, y];
-  }
-
-  // 마지막에 나온 좌표값 넣어주기
-  answer.push(cntX);
-  answer.push(cntY);
-
-  return answer;
-};
-
-console.log(solution10(["SOO", "OOO", "OOO"], ["E 2", "S 2", "W 1"]));
+}
+console.log(solution5("()(((()())(())()))(())"))
+// 이해가 너무 안돼서 삽질 너무 많이 함...
+// 막대기가 이어지다 레이저를 쏘면 레이저를 제외한 옆에 "(" 숫자만큼 잘린 막대를 얻음
+// 레이저는 아닌데 닫는 괄호가 나오면 하나를 얻음 (레이저가 포함된 막대는 다 잘랐으니 다음 막대를 자르는건 다음 레이저에게 넘김)
+// 다음 레이저가 나오면 마찬가지로 레이저를 제외한 옆에 "(" 숫자만큼 잘린 막대를 얻음
+// 레이저가 아닌데 닫는 괄호가 나오면 마찬가지로 그 막대는 다 잘린거라 하나 얻고 다음 레이저로 토스
+// 반복할 경우 답이 나옴 
