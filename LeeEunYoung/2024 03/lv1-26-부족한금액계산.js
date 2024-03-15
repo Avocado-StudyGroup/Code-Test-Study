5693e34d5d620ef38a0e6df6d4a3d7b51406c930
eun0leee <eun0leee@gmail.com>
2024-03-15 20:41:06 +0900

    240315

---
+++ LeeEunYoung/2024 03/lv1-26-부족한금액계산.js
@@ -0,0 +1,9 @@
+function solution(price, money, count) {
+  let answer = 0;
+  for (let i = 1; i <= count; i++) {
+    answer += price * i;
+  }
+  return answer - money <= 0 ? 0 : answer - money;
+}
+
+console.log(solution(3, 20, 4)); //  10
